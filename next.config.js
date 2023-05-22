3af8cde067b83aba18327a78edf7f2fc87c51cc1
maithanhtrong1113 <maithanhtrong1113@gmail.com>
2023-05-22 19:54:33 +0700

    a

--- next.config.js
+++ next.config.js
@@ -1,15 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = {
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: "https://lt-99-pharmacy.vercel.app/:path*",
+        destination:
+          "http://kl-env.eba-eyz5qutv.ap-southeast-1.elasticbeanstalk.com/api/:path*", // Thay thế bằng URL của nguồn dữ liệu HTTP
+      },
+    ];
+  },
+  async serverMiddleware() {
+    const proxy = createProxyMiddleware("/api", {
+      target: "http://kl-env.eba-eyz5qutv.ap-southeast-1.elasticbeanstalk.com", // Thay thế bằng URL của nguồn dữ liệu HTTP
+      changeOrigin: true,
+      secure: false,
+      ws: true,
+    });
+
+    return [
+      {
+        path: "/api",
+        handler: proxy,
       },
     ];
   },
 };
-
-module.exports = nextConfig;
